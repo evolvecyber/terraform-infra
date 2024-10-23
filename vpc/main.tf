00c5ccbebd449ca6957e7ebad264930ed9704a20
Evolve Cyber LLC <fsadykov@evolvecyber.com>
2024-10-23 18:55:41 -0500

    added tag

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,4 +7,7 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway = false
   enable_vpn_gateway = false
+  tags = {
+    Name = "cloud"
+  }
 }
